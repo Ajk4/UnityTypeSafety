85477b34c6b54e5e74f17712d74aeed975135b90
ajk <jakubowski.adam@gmail.com>
2020-04-18 16:45:00 +0200

    Extract scenes codegen to separate file

--- UnityTypeSafeCodegens.cs
+++ UnityTypeSafeCodegens.cs
@@ -3,7 +3,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -48,59 +47,6 @@ abstract class UnityTypeSafeCodegen<T> {
 }
 
 // TODO Refactor other codegens
-class ScenesUnityTypeSafeCodegen : UnityTypeSafeCodegen<string> {
-    protected override string Filename {
-        get { return "Scenes"; }
-    }
-
-    protected override HashSet<string> GetCurrentList() {
-        return new HashSet<string>(EditorBuildSettings.scenes.Select(scene => scene.path).Where(p => p != null && p.Length > 0));
-    }
-
-    protected override void WriteFile(StreamWriter writer, HashSet<string> list) {
-        writer.WriteLine("using UnityEngine;");
-        writer.WriteLine("using System.Linq;");
-        writer.WriteLine("using System;");
-        writer.WriteLine("");
-        writer.WriteLine("namespace UnityTypeSafety {");
-
-        writer.WriteLine("\tpublic enum Scenes {");
-        // TODO Not available scene names here. Only in runtime? Have only build index here?
-        foreach (var path in list) {
-            // This should be in getter?
-            var sceneName = Path.GetFileNameWithoutExtension(path);
-            // TODO Proper escaping
-            // TODO DRY escaping
-            var escapedName = sceneName.Replace(" ", "_").ToUpper();
-            // public static Scene DEFAULT = new Scene("default");
-            writer.WriteLine("\t\t " + escapedName + ",");
-        }
-        writer.WriteLine("\t}");
-
-        writer.WriteLine("\tpublic static class ScenesExtension {");
-        writer.WriteLine("");
-        writer.WriteLine("\t\tpublic static string GetName(this Scenes scene) {");
-        writer.WriteLine("\t\t\tswitch(scene){");
-
-        foreach (var path in list) {
-            // This should be in getter?
-            var sceneName = Path.GetFileNameWithoutExtension(path);
-            // TODO Proper escaping
-            // TODO DRY escaping
-            var escapedName = sceneName.Replace(" ", "_").ToUpper();
-            writer.WriteLine("\t\t\t\tcase Scenes." + escapedName + ":");
-            writer.WriteLine("\t\t\t\t\treturn \"" + sceneName + "\";");
-        }
-        writer.WriteLine("\t\t\t\tdefault:");
-        writer.WriteLine("\t\t\t\t\tthrow new Exception(\"Uknown \" + scene);");
-
-        writer.WriteLine("\t\t\t}");
-        writer.WriteLine("\t\t}");
-        writer.WriteLine("\t}");
-
-        writer.WriteLine("}");
-    }
-}
 
 [InitializeOnLoad]
 class UnityTypeSafeCodegens {
